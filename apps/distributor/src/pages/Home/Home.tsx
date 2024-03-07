075440df4cd31b5be5e5e5be9d140fb05a44ec57
Yash Pede <yashpede@ssipmt.com>
2024-03-07 16:15:30 +0530

    bugfix:user

--- apps/distributor/src/pages/Home/Home.tsx
+++ apps/distributor/src/pages/Home/Home.tsx
@@ -1,22 +1,21 @@
 import { Flex, Skeleton } from "antd";
-// import { useEffect } from "react";
-// import { authProvider } from "../auth/authProvider";
-// import { UseLocalStorage } from "@repo/ui";
+import { useEffect } from "react";
+import { authProvider } from "../auth/authProvider";
+import { UseLocalStorage } from "@repo/ui";
 
 export const Home = () => {
-  // const [user, setUser] = UseLocalStorage<string>("USER", "");
-  // useEffect(() => {
-  //   if (!user) return;
-  //   const userid = async () => {
-  //     if (!authProvider.getIdentity) {
-  //       return;
-  //     }
-  //     const user = await authProvider.getIdentity();
-  //     const userId = user as any;
-  //     setUser(userId);
-  //   };
-  //   userid();
-  // }, []);
+  const [user, setUser] = UseLocalStorage<string>("USER", "");
+  useEffect(() => {
+    const userid = async () => {
+      if (!authProvider.getIdentity) {
+        return;
+      }
+      const user = await authProvider.getIdentity();
+      const userId = user as any;
+      setUser(userId);
+    };
+    userid();
+  }, []);
 
   return (
     <div>
